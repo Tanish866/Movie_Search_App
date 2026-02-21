782bcd83292515f47d2345cd393dab10aea752af
Tanish Singh <tanishsingh866@gmail.com>
2026-02-21 02:33:47 +0000

    Added autocomplete ui

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useRef, useState } from 'react';
 import './Navbar.css';
 
 function Navbar(){
+    // const resultRef = useRef(null);
+    const [autoCompleteVisible, setAutoCompleteVisible] = useState(false);
     return(
         <div className="navbar-wrapper">
             <div>
@@ -8,9 +11,21 @@ function Navbar(){
             </div>
             <div className="search-bar">
                 <input 
-                    type="text" 
+                    type="text"
+                    onFocus={() => {
+                        setAutoCompleteVisible(true);
+                    }}
+                    onBlur={() => {
+                        setAutoCompleteVisible(false);
+                    }}
                     placeholder='What do you think about...'
+
                 />
+                <div id='result-list' style={{display: (autoCompleteVisible) ? 'block':'none'}}>
+                    <div className='auto-complete'>result 1</div>
+                    <div className='auto-complete'>result 2</div>
+                    <div className='auto-complete'>result 3</div>
+                </div>
             </div>
             <div>
                 Theme
